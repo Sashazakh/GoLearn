2416f8e3966585fbbabac965e557ee4b5e031cb9
Aleksandr Zakharov <apzakharov@avito.ru>
2024-04-04 08:31:04 +0200

    Tests fixes

--- Homework2/main_test.go
+++ Homework2/main_test.go
@@ -1,78 +1,90 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIsNumOrDec(t *testing.T) {
 	tests := []struct {
+		name     string
 		char     rune
 		expected bool
 	}{
-		{'0', true},
-		{'.', true},
-		{'5', true},
-		{'a', false},
-		{'+', false},
+		{name: "Zero", char: '0', expected: true},
+		{name: "Decimal Point", char: '.', expected: true},
+		{name: "Number", char: '5', expected: true},
+		{name: "Wrong Symbol", char: 'a', expected: false},
+		{name: "Plus Sign", char: '+', expected: false},
 	}
 
-	for _, testCase := range tests {
-		actual := isNumOrDec(testCase.char)
-		if actual != testCase.expected {
-			t.Errorf("Error in isNumOrDec. Char %q, got: %v, expected: %v", testCase.char, actual, testCase.expected)
-		}
+	t.Parallel()
+	for _, test := range tests {
+		tc := test
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			actual := isNumOrDec(tc.char)
+
+			assert.Equal(t, tc.expected, actual, "Error in test: %s func: isNumOrDec. Char %q, got: %v, expected: %v", tc.name, tc.char, actual, tc.expected)
+		})
 	}
 }
 
 func TestProcessCurrentNum(t *testing.T) {
 	tests := []struct {
+		name        string
 		currentNum  string
 		expected    float64
 		expectError bool
 	}{
-		{"10", 10, false},
-		{"-10", -10, false},
-		{"10.5", 10.5, false},
-		{"", defaultValue, true},
-		{"abbb", defaultValue, true},
+		{name: "Valid positive integer", currentNum: "10", expected: 10, expectError: false},
+		{name: "Valid negative integer", currentNum: "-10", expected: -10, expectError: false},
+		{name: "Valid float", currentNum: "10.5", expected: 10.5, expectError: false},
+		{name: "Empty string", currentNum: "", expected: defaultValue, expectError: true},
+		{name: "Invalid format", currentNum: "abbb", expected: defaultValue, expectError: true},
 	}
 
-	for _, testCase := range tests {
-		actualResult, err := processCurrentNum(testCase.currentNum)
-		if testCase.expectError {
-			if err == nil {
-				t.Errorf("processCurrentNum expected error for currentNum: %q but there isn't any.", testCase.currentNum)
+	for _, test := range tests {
+		tc := test
+		t.Parallel()
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			actualResult, err := processCurrentNum(tc.currentNum)
+
+			if tc.expectError {
+				assert.Error(t, err, "Expected an error for currentNum: %q", tc.currentNum)
+			} else {
+				assert.NoError(t, err, "Did not expect an error for currentNum: %q", tc.currentNum)
+				assert.Equal(t, tc.expected, actualResult, "Unexpected result for currentNum: %q", tc.currentNum)
 			}
-			continue
-		}
-		if err != nil {
-			t.Errorf("processCurrentNum error for currentNum: %q but there isn't any.", testCase.currentNum)
-		}
-		if actualResult != testCase.expected {
-			t.Errorf("processCurrentNum for num: %q returned: %v, expected: %v.", testCase.currentNum, actualResult, testCase.expected)
-		}
+		})
 	}
 }
 
 func TestEvaluateExpressionsBasic(t *testing.T) {
 	tests := []struct {
+		name       string
 		expression string
 		result     float64
 	}{
-		{"1+1", 2},
-		{"2+2", 4},
-		{"10-4", 6},
-		{"4*5", 20},
-		{"20/5", 4},
-		{"20/(2+3)", 4},
-		{"-1-1", -2},
+		{name: "Addition 1", expression: "1+1", result: 2},
+		{name: "Subtraction", expression: "10-4", result: 6},
+		{name: "Multiplication", expression: "4*5", result: 20},
+		{name: "Division", expression: "20/5", result: 4},
+		{name: "Grouped operations", expression: "20/(2+3)", result: 4},
+		{name: "Negative numbers", expression: "-1-1", result: -2},
 	}
 
-	for _, testCase := range tests {
-		actual, err := evaluateExpression(testCase.expression)
-		if err != nil {
-			t.Errorf("Error in epression: %q, error: %v", testCase.expression, err)
-		}
-		if actual != testCase.result {
-			t.Errorf("Error in calculation epression: %q, got: %v, expected: %v", testCase.expression, actual, testCase.result)
-		}
+	for _, test := range tests {
+		tc := test
+		t.Parallel()
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			actual, err := evaluateExpression(tc.expression)
+			assert.NoError(t, err, "Error evaluating expression: %q", tc.expression)
+			assert.Equal(t, tc.result, actual, "Unexpected result for expression: %q", tc.expression)
+		})
 	}
 }
